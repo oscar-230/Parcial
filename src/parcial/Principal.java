834748b800b04d2c00b01fe4f4bf411dbdcbf52e
oscar-230 <111622297+oscar-230@users.noreply.github.com>
2023-06-22 14:31:43 -0500

    Se agrega el repositorio

---
+++ src/parcial/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Oscar David Cuaical Lopez
+ * Grupo: 01
+ * Profesor: Luis Yovany Romo
+ * Fecha: 22/06/2023
+ */
+package parcial;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
